ada32d148c41845ace0e523dd89041dfbbd09e53
Keshav Bhandari <keshavbhandari@gmail.com>
2024-05-01 12:44:57 +0100

    Update requirements.txt and .gitignore, delete unused config files

---
+++ fusion/build_vocab.py
@@ -0,0 +1,58 @@
+import yaml
+import os
+import argparse
+import pickle
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", type=str, default=os.path.normpath("/homes/kb658/yinyang/configs/configs_os.yaml"),
+                    help="Path to the config file")
+args = parser.parse_args()
+
+# Load config file
+with open(args.config, 'r') as f:
+    configs = yaml.safe_load(f)
+
+artifact_folder = configs["raw_data"]["artifact_folder"]
+mono_folder = configs["raw_data"]["mono_folder"]
+json_folder = configs["raw_data"]["json_folder"]
+raw_data_folders = configs["raw_data"]["raw_data_folders"]
+data_path = raw_data_folders['classical']['folder_path']
+
+# Build the vocabulary
+vocab = {}
+# MIDI velocity range from 0 to 127
+velocity = [0, 15, 30, 45, 60, 75, 90, 105, 120, 127]
+# MIDI pitch range from 0 to 127
+midi_pitch = list(range(0, 128))
+# Onsets are quantized in 10 milliseconds up to 5 seconds
+onset = list(range(0, 5001, 10))
+duration = list(range(0, 5001, 10))
+
+# Add the tokens to the vocabulary
+for v in velocity:
+    for p in midi_pitch:
+        vocab[("piano", p, v)] = len(vocab)
+for o in onset:
+    vocab[("onset", o)] = len(vocab)
+for d in duration:
+    vocab[("dur", d)] = len(vocab)
+
+# Special tokens
+vocab[('prefix', 'instrument', 'piano')] = len(vocab)
+vocab["<T>"] = len(vocab)
+vocab["<S>"] = len(vocab)
+vocab["<E>"] = len(vocab)
+vocab["SEP"] = len(vocab)
+
+# Print the vocabulary length
+print(f"Vocabulary length: {len(vocab)}")
+
+# Save the vocabulary
+# Directory path
+tokenizer_folder = os.path.join(artifact_folder, "fusion")
+# Make directory if it doesn't exist
+os.makedirs(tokenizer_folder, exist_ok=True)
+vocab_path = os.path.join(tokenizer_folder, "vocab.pkl")
+with open(os.path.join(tokenizer_folder, "vocab.pkl"), 'wb') as f:
+    pickle.dump(vocab, f)
--- fusion/data_loader.py
+++ fusion/data_loader.py
@@ -1,6 +1,7 @@
 import yaml
 import jsonlines
 import glob
+import random
 from aria.data.midi import MidiDict
 from aria.tokenizer import AbsTokenizer
 
@@ -21,7 +22,10 @@ midi_files = glob.glob(data_path + '/**/*.midi', recursive=True)
 
 
 tokenizer = AbsTokenizer()
-midi_file_path = midi_files[0]
+midi_file_path = random.choice(midi_files)
+midi_file_path = "/import/c4dm-datasets/maestro-v3.0.0/2008/MIDI-Unprocessed_07_R2_2008_01-05_ORIG_MID--AUDIO_07_R2_2008_wav--2.midi"
+print(midi_file_path)
+# midi_file_path = midi_files[0]
 mid = MidiDict.from_midi(midi_file_path)
 tokenized_sequence = tokenizer.tokenize(mid)
 
@@ -43,63 +47,112 @@ def flatten(sequence):
             flattened_sequence.append(note_info) 
             note_info = []
 
-    return flattened_sequence  
+    return flattened_sequence
 
 
-# Call the flatten function
-flattened_sequence = flatten(tokenized_sequence)
-print(flattened_sequence[0:100])
-
-
-def skyline(sequence: list):
+def skyline(sequence: list, diff_threshold=50):
     melody = []
-    pitch = 0
-    velocity = sequence[0][1]
-    onset = sequence[0][2]
-    duration = sequence[0][3]
+    harmony = []
+    pointer_pitch = sequence[0][0]
+    pointer_velocity = sequence[0][1]
+    pointer_onset = sequence[0][2]
+    pointer_duration = sequence[0][3]
+    melody_onset_duration_tracker = pointer_onset + pointer_duration
+    harmony_onset_duration_tracker = pointer_onset + pointer_duration
     i = 0
 
-    for i in range(len(sequence)):
+    for i in range(1, len(sequence)):
         if type(sequence[i]) != str:
             current_pitch = sequence[i][0]
             current_velocity = sequence[i][1]
             current_onset = sequence[i][2]
             current_duration = sequence[i][3]
 
-            # Get next note onset
-            if i+1 < len(sequence):
-                next_onset = sequence[i+1][2]
-            
-            # Check if current_onset is different from next onset
-            if current_onset != next_onset:
-                if current_onset == onset and current_pitch > pitch:
-                    pitch = current_pitch
-                    onset = current_onset
-                    duration = current_duration
-                    velocity = current_velocity
-                else:
-                    pitch = current_pitch
-                    onset = current_onset
-                    duration = current_duration
-                    velocity = current_velocity
-                # Append the last note
-                melody.append(("piano", pitch, velocity))
-                melody.append(("onset", onset))
-                melody.append(("dur", duration))
-
+            if type(sequence[i-1]) == str and type(sequence[i-2]) == str:
+                diff_curr_prev_onset = 5000
+            elif type(sequence[i-1]) == str and type(sequence[i-2]) != str:
+                diff_curr_prev_onset = abs(current_onset - sequence[i-2][2])
             else:
-                if current_pitch > pitch:
-                    pitch = current_pitch
-                    onset = current_onset
-                    duration = current_duration
-                    velocity = current_velocity
+                diff_curr_prev_onset = abs(current_onset - sequence[i-1][2])
+
+            if diff_curr_prev_onset > diff_threshold:
+                # Append <t> based on condition
+                if melody_onset_duration_tracker > 5000 and pointer_onset + pointer_duration < 5000:
+                    melody.append("<T>")
+                # Append the previous note
+                melody.append(("piano", pointer_pitch, pointer_velocity))
+                melody.append(("onset", pointer_onset))
+                melody.append(("dur", pointer_duration))
+                melody_onset_duration_tracker = pointer_onset + pointer_duration
+                # Update the pointer
+                pointer_pitch = current_pitch
+                pointer_velocity = current_velocity
+                pointer_onset = current_onset
+                pointer_duration = current_duration
+            else:
+                if current_pitch > pointer_pitch:
+                     # Append <t> based on condition
+                    if harmony_onset_duration_tracker > 5000 and pointer_onset + pointer_duration < 5000:
+                        harmony.append("<T>")
+                    # Append the previous note
+                    harmony.append(("piano", pointer_pitch, pointer_velocity))
+                    harmony.append(("onset", pointer_onset))
+                    harmony.append(("dur", pointer_duration))
+                    harmony_onset_duration_tracker = pointer_onset + pointer_duration
+                    # Update the pointer
+                    pointer_pitch = current_pitch
+                    pointer_velocity = current_velocity
+                    pointer_onset = current_onset
+                    pointer_duration = current_duration
                 else:
+                    # Append <t> based on condition
+                    if harmony_onset_duration_tracker > 5000 and pointer_onset + pointer_duration < 5000:
+                        harmony.append("<T>")
+                    # Append the previous note
+                    harmony.append(("piano", current_pitch, current_velocity))
+                    harmony.append(("onset", current_onset))
+                    harmony.append(("dur", current_duration))
+                    harmony_onset_duration_tracker = current_onset + current_duration
                     continue
-        else:
-            melody.append(sequence[i])
-    return melody
-                
+
+            # Append the last note
+            if i == len(sequence) - 1: 
+                if diff_curr_prev_onset > diff_threshold:
+                    melody.append(("piano", pointer_pitch, pointer_velocity))
+                    melody.append(("onset", pointer_onset))
+                    melody.append(("dur", pointer_duration))
+                else:
+                    if current_pitch > pointer_pitch:
+                        melody.append(("piano", current_pitch, current_velocity))
+                        melody.append(("onset", current_onset))
+                        melody.append(("dur", current_duration))
+                    else:
+                        harmony.append(("piano", current_pitch, current_velocity))
+                        harmony.append(("onset", current_onset))
+                        harmony.append(("dur", current_duration))
+
+    return melody, harmony
+
+
+# Call the flatten function
+flattened_sequence = flatten(tokenized_sequence)
+print(flattened_sequence[0:100], '\n')
 
 # Call the skyline function
-melody = skyline(flattened_sequence[0:100])
+melody, harmony = skyline(flattened_sequence, diff_threshold=30)
+melody = tokenized_sequence[0:2] + melody + tokenized_sequence[-1:]
+harmony = tokenized_sequence[0:2] + harmony + tokenized_sequence[-1:]
 print(melody[0:100], '\n')
+print(harmony[0:100], '\n')
+
+print(len(tokenized_sequence))
+print(len(melody))
+print(len(harmony))
+
+# mid_dict = tokenizer.detokenize(melody) # mid_dict is a MidiDict object
+# mid = mid_dict.to_midi() # mid is a mido.MidiFile
+# mid.save('test_file_melody.mid')
+
+# mid_dict = tokenizer.detokenize(harmony) # mid_dict is a MidiDict object
+# mid = mid_dict.to_midi() # mid is a mido.MidiFile
+# mid.save('test_file_harmony.mid')
